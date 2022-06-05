81e19059d2e4e7a35080abf03602f493cc6467c0
Elie Choi <100371808+elianachoi@users.noreply.github.com>
2022-06-05 01:37:31 -0400

    a

---
+++ script.js
@@ -0,0 +1,8 @@
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+      alert("hi");
+ });
