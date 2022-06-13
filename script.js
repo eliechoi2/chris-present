4a5ff30097c847b96eb6a05456e8e6fe5bec06b6
Elie Choi <100371808+elianachoi@users.noreply.github.com>
2022-06-13 03:25:10 -0400

    a

--- script.js
+++ script.js
@@ -149,3 +149,29 @@ function photos() {
     gallery.style.display = "none";
   }
 }
+
+
+const cursor = document
+    .querySelector(".custom-cursor");
+
+// Adding the animations when the
+// mouse button is clicked
+
+window.addEventListener("mousedown", (event) => {
+    if (!cursor.classList.contains("click")) {
+        cursor.classList.add("click");
+
+        setTimeout(() => {
+            cursor.classList.remove("click");
+        }, 800);
+    }
+});
+
+// Getting the position of the cursor
+window.addEventListener("mousemove", (event) => {
+    let x = event.pageX - cursor.offsetWidth / 2,
+        y = event.pageY - cursor.offsetHeight / 2;
+
+    cursor.style.left = `${x}px`;
+    cursor.style.top = `${y}px`;
+});
