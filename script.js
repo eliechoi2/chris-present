aef59fafea7803cde726897b42d7da33fed69ece
Elie Choi <100371808+elianachoi@users.noreply.github.com>
2022-06-19 23:30:05 -0400

    a

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const button = document.getElementById("button");
 
 button.onclick = function() {
   if (document.getElementById("username").value == "chris" && document.getElementById("password").value == "iloveyou") {
-    document.location = ("https://chris-present.herokuapp.com/about.html");
+    document.location = ("https://chris-present.herokuapp.com/home.html");
   } else {
     alert("Incorrect username/password. Please try again.");
   }
